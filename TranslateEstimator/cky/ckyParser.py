a56a8e4f7ea68a61405979232c163ee21b47bea4
eladco <eladcoo@gmail.com>
2019-08-17 21:28:38 +0300

    adding unknown words

--- TranslateEstimator/cky/ckyParser.py
+++ TranslateEstimator/cky/ckyParser.py
@@ -1,19 +1,43 @@
 from functools import reduce
 import sys, time
 
+from nltk import induce_pcfg
 from nltk import tokenize
+from nltk import treetransforms
 import nltk
 from nltk.corpus import treebank
-from nltk.grammar import CFG, Nonterminal
+from nltk.grammar import PCFG, Nonterminal
 from nltk.parse import ViterbiParser
 from nltk.parse import pchart
-from TranslateEstimator.training.eng_grammar import createGrammar
 
 
-tbank_grammar = createGrammar()
+def get_missing_words(grammar, words):
+    """
+    Find list of missing words not covered by grammar
+    """
+    missing = [word for word in words
+               if not grammar._lexical_index.get(word)]
+    return missing
 
-demos = [('I saw John with my telescope', tbank_grammar)]
-sent, grammar = demos[0]
+
+def createGrammar(unkownWords):
+    
+    productions = []
+    for item in treebank.fileids()[:2]:
+        for tree in treebank.parsed_sents(item):
+            # perform optional tree transformations, e.g.:
+            tree.collapse_unary(collapsePOS=False)  # Remove branches A-B-C into A-B+C
+            tree.chomsky_normal_form(horzMarkov=2)  # Remove A->(B,C,D) into A->B,C+D->D
+            productions += tree.productions()
+    for word in unkownWords:
+        print(word)
+        productions.append(nltk.grammar.Production(Nonterminal('NN'), (word,)))
+    return induce_pcfg(Nonterminal('S'), list(productions))
+
+
+# def parseSentence(sentence):
+sent = 'I saw John with my telescope'
+grammar = createGrammar(set())
 
 # Tokenize the sentence.
 tokens = sent.split()
@@ -23,21 +47,15 @@ times = []
 average_p = []
 num_parses = []
 all_parses = {}
+uknown_words = get_missing_words(grammar, 'I saw John with my telescope'.split())
+
+if(uknown_words != []):
+    grammar = createGrammar(uknown_words)
+
 parser = ViterbiParser(grammar)
 print('\ns: %s\nparser: %s\ngrammar: %s' % (sent, parser, grammar))
 parser.trace(3)
-t = time.time()
 parses = parser.parse_all(tokens)
-times.append(time.time() - t)
-if parses: 
-    lp = len(parses)
-    p = reduce(lambda a, b:a + b.prob(), parses, 0.0)
-else: 
-    p = 0
-average_p.append(p)
-num_parses.append(len(parses))
-for p in parses: 
-    all_parses[p.freeze()] = 1
 
 for parse in parses:
     print(parse)
