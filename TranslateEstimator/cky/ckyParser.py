f06dbf9c69acddfcaeb656d264a1b035f9a6ab46
eladco <eladcoo@gmail.com>
2019-08-23 21:05:00 +0300

    fix eclipse packages error

--- TranslateEstimator/cky/ckyParser.py
+++ TranslateEstimator/cky/ckyParser.py
@@ -1,61 +1,99 @@
 from functools import reduce
 import sys, time
-
+import nltk
 from nltk import induce_pcfg
 from nltk import tokenize
 from nltk import treetransforms
-import nltk
+from nltk import pos_tag
 from nltk.corpus import treebank
-from nltk.grammar import PCFG, Nonterminal
+from nltk.grammar import PCFG, Nonterminal, toy_pcfg1
 from nltk.parse import ViterbiParser
 from nltk.parse import pchart
 
 
-def get_missing_words(grammar, words):
-    """
-    Find list of missing words not covered by grammar
-    """
-    missing = [word for word in words
-               if not grammar._lexical_index.get(word)]
-    return missing
+class CKYParser:
 
-
-def createGrammar(unkownWords):
+    def get_missing_words(self, grammar, words):
+        """
+        Find list of missing words not covered by grammar
+        """
+        missing = [word for word in words
+                   if not grammar._lexical_index.get(word)]
+        return missing
     
-    productions = []
-    for item in treebank.fileids()[:2]:
-        for tree in treebank.parsed_sents(item):
+    def createGrammar(self, unkownWords):
+        print("Induce PCFG grammar from treebank data:")
+        productions = []
+        for item in treebank.fileids():
+          for tree in treebank.parsed_sents(item):
             # perform optional tree transformations, e.g.:
             tree.collapse_unary(collapsePOS=False)  # Remove branches A-B-C into A-B+C
             tree.chomsky_normal_form(horzMarkov=2)  # Remove A->(B,C,D) into A->B,C+D->D
             productions += tree.productions()
-    for word in unkownWords:
-        print(word)
-        productions.append(nltk.grammar.Production(Nonterminal('NN'), (word,)))
-    return induce_pcfg(Nonterminal('S'), list(productions))
+        for word in unkownWords:
+            tag = pos_tag([word])[0][1]
+            productions.append(nltk.grammar.Production(Nonterminal(tag), (word,)))
+        S = Nonterminal('S')
+        grammar = induce_pcfg(S, productions)
+        return grammar
 
-
-# def parseSentence(sentence):
-sent = 'I saw John with my telescope'
-grammar = createGrammar(set())
-
-# Tokenize the sentence.
-tokens = sent.split()
-
-# Run the parsers on the tokenized sentence.
-times = []
-average_p = []
-num_parses = []
-all_parses = {}
-uknown_words = get_missing_words(grammar, 'I saw John with my telescope'.split())
-
-if(uknown_words != []):
-    grammar = createGrammar(uknown_words)
-
-parser = ViterbiParser(grammar)
-print('\ns: %s\nparser: %s\ngrammar: %s' % (sent, parser, grammar))
-parser.trace(3)
-parses = parser.parse_all(tokens)
-
-for parse in parses:
-    print(parse)
+    def parseSentence(self, sent):
+        
+        grammar = self.createGrammar(set())
+                
+        # Tokenize the sentence.
+        words = sent.split()
+        
+        unknown_words = self.get_missing_words(grammar, words);
+        
+        if(unknown_words != []):
+            grammar = self.createGrammar(unknown_words)
+        # Define a list of parsers.  We'll use all parsers.
+        parsers = [
+        ViterbiParser(grammar),
+        ]
+        
+        times = []
+        average_p = []
+        num_parses = []
+        all_parses = {}
+        for parser in parsers:
+            print('\ns: %s\nparser: %s\ngrammar: %s' % (sent, parser, grammar))
+            parser.trace(3)
+            t = time.time()
+            parses = parser.parse_all(words)
+            times.append(time.time() - t)
+            if parses: 
+                lp = len(parses)
+                p = reduce(lambda a, b:a + b.prob(), parses, 0.0)
+            else: 
+                p = 0
+            average_p.append(p)
+            num_parses.append(len(parses))
+            for p in parses: 
+                all_parses[p.freeze()] = 1
+        
+        # Print summary statistics
+        print()
+        print('-------------------------+------------------------------------------')
+        print('   Parser           Beam | Time (secs)   # Parses   Average P(parse)')
+        print('-------------------------+------------------------------------------')
+        for i in range(len(parsers)):
+            print('%19s %4d |%11.4f%11d%19.14f' % (parsers[i].__class__.__name__,
+              getattr(parsers[0], "beam_size", 0),
+              times[i],
+              num_parses[i],
+              average_p[i]))
+        parses = all_parses.keys()
+        if parses: 
+            p = reduce(lambda a, b:a + b.prob(), parses, 0) / len(parses)
+        else: 
+            p = 0
+        print('-------------------------+------------------------------------------')
+        print('%19s      |%11s%11d%19.14f' % ('(All Parses)', 'n/a', len(parses), p))
+        print()
+        
+        for parse in parses:
+            print(parse)
+        
+        return parse.pos()
--- TranslateEstimator/evaluation/solution.py
+++ TranslateEstimator/evaluation/solution.py
@@ -1,8 +1,39 @@
-from TranslateEstimator.yap.yapParser import parse
-from TranslateEstimator.google_api.translator import GoogleTranslator
-from TranslateEstimator.TED.tedTalks import init_ted_talks
-class Solution:
-    init_ted_talks()
-    sentence = "I want to sleep"
-    translated = GoogleTranslator.translate(sentence, 'en', 'he')
-    parse(translated.text)
+from yap.yapParser import parse
+from google_api.translator import GoogleTranslator
+from cky.ckyParser import CKYParser
+
+
+class Evaluator:
+#     init_ted_talks()
+#     sentence = "I want to sleep"
+#     translated = GoogleTranslator.translate(sentence, 'en', 'he')
+#     parse(translated.text)
+#     evaluate_eng_to_heb('','I saw a telescope');
+    parser = CKYParser()
+    eng_tag = parser.parseSentence('Elad saw 8 telescopes')
+    print(eng_tag)
+    
+    def evaluate_eng_to_heb(self, heb_sent, eng_sent):
+        parser = CKYParser()
+        eng_tag = parser.parseSentence(eng_sent)
+        heb_tag = []  # cky_of_an_hebrew_sentence(eng_sent)
+        self.evaluate_pos_tagging(eng_tag, heb_tag)
+     
+    def evaluate_pos_tagging(self, src_tag, dst_tag):
+        score = 0
+        num_of_parameters = 0;
+        
+        numOfSrcVerbs = sum(p[0] in ('VB', 'VBD', 'VBG', 'VGN', 'VBP', 'VBZ') for p in src_tag)
+        num_of_parameters += numOfSrcVerbs;
+        numOfDstVerbs = sum(p[0] == 'VB' for p in dst_tag)
+        score += numOfDstVerbs
+        
+        numOfSrcPronouns = sum(p[0] in ('PRP') for p in src_tag)
+        num_of_parameters += numOfSrcPronouns;
+        numOfDstPronouns = sum(p[0] == 'PRP' for p in dst_tag)
+        score += numOfDstPronouns
+        
+        numOfSrcNums = sum(p[0] in ('CD') for p in src_tag)
+        num_of_parameters += numOfSrcNums;
+        numOfDstNums = sum(p[0] == 'CD' for p in dst_tag)
+        score += numOfDstNums
