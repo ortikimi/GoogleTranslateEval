ff1b1bd2aa6f7f6db73c6b478f56fd66dcccf152
eladco <eladcoo@gmail.com>
2019-09-21 21:37:50 +0300

    add scoring params

--- TranslateEstimator/evaluation/translateEvaluator.py
+++ TranslateEstimator/evaluation/translateEvaluator.py
@@ -9,8 +9,8 @@ class TranslateEvaluator:
     def evaluate_pos_tagging(self, src_tags, dst_tags):
         eval_of_dst = 0
         eval_of_src = 0
-        src_tags_output = ""
-        dst_tags_output = ""
+        src_tags_output = "Source: \n"
+        dst_tags_output = "Destination: \n"
         listOfTags = [
             {
                 'Tag':'VERB',
@@ -88,17 +88,17 @@ class TranslateEvaluator:
         
         for tag in listOfTags:
             numOfSrcTags = sum(item[1] in tag[self.source_language] for item in src_tags)
-            src_tags_output = src_tags_output + "#" + tag['Tag'] + " : " + numOfSrcTags + "\n";
+            src_tags_output = src_tags_output + "#" + tag['Tag'] + " : " + str(numOfSrcTags) + "\n";
             eval_of_src += tag['Weight'] * numOfSrcTags
             numOfDstTags = sum(item[1] in tag[self.destination_language] for item in dst_tags)
-            dst_tags_output = dst_tags_output + "#" + tag['Tag'] + " : " + numOfDstTags + "\n";
+            dst_tags_output = dst_tags_output + "#" + tag['Tag'] + " : " + str(numOfDstTags) + "\n";
             eval_of_dst += tag['Weight'] * numOfDstTags
         
         if(eval_of_src == 0 or eval_of_dst == 0):
             return 0
         else: 
             if (eval_of_src < eval_of_dst):
-                return {"score": eval_of_src / eval_of_dst, "source_tags":  src_tags_output, "dest_tags": dst_tags_output}
+                return {'score': eval_of_src / eval_of_dst, 'num_of_tags':  src_tags_output + dst_tags_output}
             else:
-                return {"score": eval_of_dst / eval_of_src, "source_tags":  src_tags_output, "dest_tags": dst_tags_output}
+                return {'score': eval_of_dst / eval_of_src, 'num_of_tags':  src_tags_output + dst_tags_output}
 
