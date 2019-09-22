858aa22bec52c11dc2fc2de9473fdec62718fdb2
eladco <eladcoo@gmail.com>
2019-09-22 22:28:42 +0300

    fixing the score

--- TranslateEstimator/evaluation/translateEvaluator.py
+++ TranslateEstimator/evaluation/translateEvaluator.py
@@ -11,94 +11,68 @@ class TranslateEvaluator:
         eval_of_src = 0
         src_tags_output = "Source: \n"
         dst_tags_output = "Destination: \n"
-        listOfTags = [
-            {
-                'Tag':'VERB',
-                'Weight': 1,
-                'en': ('VB', 'VBG', 'VBN', 'MD'),
-                'he' : ('VB', 'BN', 'MD')
-                },
-            {
-                'Tag':'PRONOUN',
-                'Weight': 0.5,
-                'en': ('PRP', ' PRP$'),
-                'he' : ('PRP', ' S_PRN')
-                },
-            {
-                'Tag':'NAME',
-                'Weight': 1,
-                'en': ('NNP', 'NNPS'),
-                'he' : ('NNP',)
-                },
-            {
-                'Tag':'COP',
-                'Weight': 0.5,
-                'en': ('VBD', 'VBZ', 'VBP'),
-                'he' : ('COP',)
-                },
-            {
-                'Tag':'DET',
-                'Weight': 0.1,
-                'en': ('DT',),
-                'he' : ('REL', 'DEF')
-                },
-            {
-                'Tag':'NUMBER',
-                'Weight': 1,
-                'en': ('CD'),
-                'he' : ('CD', 'NCD')
-                },
-            {
-                'Tag':'NOUN',
-                'Weight': 0.2,
-                'en': ('NN', 'NNS'),
-                'he' : ('NN', ' NNT')
-                },
-            {
-                'Tag':'ADJECTIVE',
-                'Weight': 0.2,
-                'en': ('JJ', 'JJS'),
-                'he' : ('JJ',)
-                },
-            {
-                'Tag':'RB',
-                'Weight': 0.2,
-                'en': ('RB',),
-                'he' : ('RB',)
-                },
-            {
-                'Tag':'ADP',
-                'Weight': 0.1,
-                'en': ('IN',),
-                'he' : ('IN', 'POS', 'PREPOSITION')
-                },
-            {
-                'Tag':'AND',
-                'Weight': 0.1,
-                'en': ('CC',),
-                'he' : ('CONJ',)
-                },
-            {
-                'Tag':'EXIST',
-                'Weight': 0.1,
-                'en': ('EX',),
-                'he' : ('EX',)
-                }
-        ]
+        listOfHeTags = {
+            'VB':1,
+            'MD':1,
+            'BN':1,
+            'PRP':0.5,
+            'S_PRN':0.5,
+            'NNP':1,
+            'COP':0.5,
+            'REL':0.1,
+            'DEF':0.1,
+            'CD':1,
+            'NCD':1,
+            'NN':0.2,
+            'NNT':0.2,
+            'JJ':0.2,
+            'RB':0.2,
+            'IN':0.1,
+            'POS':0.1,
+            'PREPOSITION':0.1,
+            'CONJ':0.1,
+            'EX':0.1
+        }
+    
+        HeToEnTag = {
+        'VB':['VB', 'VBG', 'VBN', 'MD'],
+        'MD':['VB', 'VBG', 'VBN', 'MD'],
+        'BN':['VB', 'VBG', 'VBN', 'MD'],
+        'PRP':['PRP', ' PRP$'],
+        'S_PRN':['PRP', ' PRP$'],
+        'NNP':['NNP', 'NNPS'],
+        'COP':['VBD', 'VBZ', 'VBP'],
+        'REL':['DT', ],
+        'DEF':['DT', ],
+        'CD':['CD', ],
+        'NCD':['CD', ],
+        'NN':['NN', 'NNS'],
+        'NNT':['NN', 'NNS'],
+        'JJ':['JJ', 'JJS'],
+        'RB':['RB', ],
+        'IN':['IN', ],
+        'POS':['IN', ],
+        'PREPOSITION':['IN', ],
+        'CONJ':['CC', ],
+        'EX':['EX', ]
+    }
         
-        for tag in listOfTags:
-            numOfSrcTags = sum(item[1] in tag[self.source_language] for item in src_tags)
-            src_tags_output = src_tags_output + "#" + tag['Tag'] + " : " + str(numOfSrcTags) + "\n";
-            eval_of_src += tag['Weight'] * numOfSrcTags
-            numOfDstTags = sum(item[1] in tag[self.destination_language] for item in dst_tags)
-            dst_tags_output = dst_tags_output + "#" + tag['Tag'] + " : " + str(numOfDstTags) + "\n";
-            eval_of_dst += tag['Weight'] * numOfDstTags
+        # Iterate over the tags of the source sentence - skip repeated tags.
+        listOfUniqueSrcTags = set(map(lambda tag: tag[1], src_tags))
+        
+        for tag in listOfUniqueSrcTags:
+            if(tag in listOfHeTags.keys()):
+                numOfSrcTags = sum(item[1] == tag for item in src_tags)
+                src_tags_output = src_tags_output + "#" + tag + " : " + str(numOfSrcTags) + "\n";
+                eval_of_src += listOfHeTags[tag] * numOfSrcTags
+                numOfDstTags = sum(item[1] in HeToEnTag[tag] for item in dst_tags)
+                dst_tags_output = dst_tags_output + "#" + tag + " : " + str(numOfDstTags) + "\n";
+                eval_of_dst += listOfHeTags[tag] * numOfDstTags
         
         if(eval_of_src == 0 or eval_of_dst == 0):
-            return 0
+            return {'score': 0, 'num_of_tags':  src_tags_output + dst_tags_output}
         else: 
             if (eval_of_src < eval_of_dst):
                 return {'score': eval_of_src / eval_of_dst, 'num_of_tags':  src_tags_output + dst_tags_output}
             else:
                 return {'score': eval_of_dst / eval_of_src, 'num_of_tags':  src_tags_output + dst_tags_output}
-
